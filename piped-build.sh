2d03fdcbecc0e65825169d583d7f9fe73d3e6aad
Nexxkinn <2881204-Nexxkinn@users.noreply.gitlab.com>
2022-10-20 22:55:35 +0700

    initial piped build script

--- piped-build.sh
+++ piped-build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install required packages to build
-apk --no-cache yarn openjdk17-jdk go build-base libwebp-dev
+apk --no-cache add nginx yarn openjdk17-jdk go build-base libwebp-dev
 
 # Piped frontend
 export NODE_OPTIONS=--max_old_space_size=1536
@@ -11,16 +11,17 @@ echo "Compiling Piped frontend, might take a while"
 
 rm -rf piped-fe.zip ./Piped-master/ ./piped-fe/
 wget -O piped-fe.zip https://github.com/TeamPiped/Piped/archive/refs/heads/master.zip
-unzip piped-fe.zip && mv Piped-master piped-fe
+unzip piped-fe.zip && mv Piped-master piped-fe && rm piped-fe.zip
 
 pushd piped-fe
 yarn install --prefer-offline
 yarn build
-mkdir -p /usr/share/nginx/html && mv dist/ /usr/share/nginx/html/
-cp docker/nginx.conf ../config/piped-fe.conf
+rm -rf /usr/share/nginx/html/piped/
+mkdir -p /usr/share/nginx/html/piped/ && mv dist/** /usr/share/nginx/html/piped/
+# cp docker/nginx.conf ../config/piped-fe.conf
 popd
 
-rm -rf piped-fe
+rm -rf piped-fe /usr/local/share/.cache/yarn/
 
 # Piped backend
 wget -O piped-be.zip https://github.com/TeamPiped/Piped-Backend/archive/refs/heads/master.zip
@@ -33,8 +34,7 @@ cp build/libs/piped-1.0-all.jar ../piped-server.jar
 cp config.properties ../
 popd
 
-rm -rf ./piped-be
-rm -rf ./.gradle
+rm -rf ./.gradle ./piped-be
 
 # ytproxy
 echo "Compiling Piped ytproxy, might take a while"
@@ -46,5 +46,6 @@ go build -ldflags "-s -w" main.go
 popd
 
 mv piped-ytproxy/main ./piped-proxy
-rm -rf ./piped-ytproxy/ ./go/
+rm -rf ./piped-ytproxy/ ./go/ ./.cache/go-build
 
+apk del yarn openjdk17-jdk go build-base libwebp-dev
